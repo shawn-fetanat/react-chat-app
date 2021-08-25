541fbbe2f0e84169e02601b6cb9378aac6c7b207
Shawn Fetanat <shawn.fetanat@gmail.com>
2021-08-25 06:57:28 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import LandingPage from "./pages/LandingPage";
-import SignUpForm from "./components/SignUpForm";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "963412d5-7733-4486-a58f-114187c72fe0";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
-    <Router>
-      <Route path="/" exact={true} component={LandingPage} />
-      <Route path="/signup" exact={true} component={SignUpForm} />
-    </Router>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
   );
 };
-
-// infinite scroll, logout, more customizations...
 
 export default App;
